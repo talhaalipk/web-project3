75582252d9574d7fb298738b5379d4f6ce6cc4c5
Talha Ali <[200~talhaali1234056@gmail.com>
2024-03-22 12:20:37 +0500

    Completed For desktop & tablet

---
+++ js/script.js
@@ -0,0 +1,99 @@
+let numOfTaskCreated;
+let numOfTaskDone;
+let zeroTaskHtml = `<img class="imageStyle" src="./assets/images/noTask.png" alt="Not have task img" /><h2 class="h2Style">You don't have tasks registered yet</h2><h2 class="h2Style">Create tasks and organize your to-do items</h2>`;
+const taskCreaterEle = document.querySelector("#taskCreater");
+const taskCompletedEle = document.querySelector("#taskDone");
+const taskContainer = document.querySelector("#taskContainer");
+const addButton = document.querySelector("#addButton");
+const input = document.querySelector("#input");
+const errorText = document.querySelector("#errorText");
+
+// console.log(taskCreaterEle);
+// console.log(taskCompletedEle);
+// console.log(taskContainer);
+// console.log(addButton);
+
+function ZeroTaskContent() {
+  taskContainer.innerHTML = zeroTaskHtml;
+  taskContainer.classList.add("center");
+}
+
+function startUp() {
+  numOfTaskCreated = 0;
+  numOfTaskDone = 0;
+  taskCreaterEle.innerHTML = numOfTaskCreated;
+  taskCompletedEle.innerHTML = numOfTaskDone;
+  ZeroTaskContent();
+}
+
+function addnewTask(value) {
+  let tempContainer = document.createElement("div");
+  tempContainer.innerHTML = `<div class="singleTask"><img class="disable" src="./assets/images/notFill.png" alt="not done" /><div class="content"><p>${value}</p></div><img class="deleteButton" src="./assets/images/delete.png" alt="delete" /></div>`;
+
+  let newtask = tempContainer;
+
+  if (numOfTaskCreated === 0) {
+    taskContainer.innerHTML = "";
+    taskContainer.classList.remove("center");
+  }
+
+  taskContainer.prepend(newtask);
+  numOfTaskCreated++;
+  taskCreaterEle.innerHTML = numOfTaskCreated;
+  taskCompletedEle.innerHTML = `${numOfTaskDone} of ${numOfTaskCreated}`;
+
+  newtask.querySelector(".disable").addEventListener("click", function (e) {
+    let fillCircleImg = document.createElement("img");
+    fillCircleImg.src = "./assets/images/FillCircle.png";
+    fillCircleImg.alt = "completed";
+    e.target.replaceWith(fillCircleImg);
+    // console.log(fillCircleImg.nextSibling);
+    let para = fillCircleImg.nextSibling.querySelector("p");
+    // console.log(para);
+    let content = para.innerHTML;
+    para.innerHTML = `<s>${content}</s>`;
+    numOfTaskDone++;
+    taskCompletedEle.innerHTML = `${numOfTaskDone} of ${numOfTaskCreated}`;
+  });
+
+  newtask
+    .querySelector(".deleteButton")
+    .addEventListener("click", function (e) {
+      console.log(e.target.parentNode);
+      e.target.parentNode.remove();
+      // removeElement(e.target.parentNode);
+      numOfTaskCreated--;
+      numOfTaskDone--;
+
+      if (numOfTaskCreated === 0) {
+        startUp();
+      }
+      taskCompletedEle.innerHTML = `${numOfTaskDone} of ${numOfTaskCreated}`;
+    });
+}
+
+async function clearErrorTextWithDelay() {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 300);
+  });
+  errorText.innerHTML = "";
+}
+
+//task add lisner
+addButton.addEventListener("click", (e) => {
+  let value = input.value;
+  input.value = "";
+  // console.log(typeof input.value);
+
+  if (value === "") {
+    errorText.innerHTML = "Invalid Input";
+    clearErrorTextWithDelay();
+  } else {
+    addnewTask(value);
+  }
+});
+
+//Calling functions
+startUp();
---
+++ readme.md
@@ -0,0 +1,20 @@
+### live Link "https://talhaalipk.github.io/web-project3/"
+
+# To-Do App
+
+A straightforward to-do list web application built with HTML, CSS, and JavaScript. Users can seamlessly add tasks, mark them as completed, and remove them as needed.
+
+## Features
+
+- **Add Tasks**: Enter your task in the input field and click "Add" to add it to the list.
+- **Mark as Completed**: Click on the empty circle icon next to a task to mark it as completed.
+- **Delete Tasks**: Remove unwanted tasks from the list by clicking on the delete icon.
+- **Error Handling**: Provides feedback for invalid input to enhance user experience.
+
+
+## Usage
+
+1. Enter a task in the input field.
+2. Click "Add" to add the task to the list.
+3. To mark a task as completed, click on the empty circle icon.
+4. To delete a task, click on the delete icon.
